8dfcc2434c378855836517d5e934364e0f357229
Carla-coder <cmartezanato@yahoo.com.br>
2024-01-24 18:31:04 -0300

    Att

--- 2DES/1-pwbe/aula1/banco/script.sql
+++ 2DES/1-pwbe/aula1/banco/script.sql
@@ -1,4 +1,4 @@
---DDL - Estrutura do banco de daos
+--DDL - Estrutura do banco de dados
 drop database if exists lojinha;
 create database lojinha;
 use lojinha;
@@ -12,11 +12,12 @@ create table Clientes(
 describe Clientes;
 
 --DML - Popular com dados de teste
-insert into Clientes(cpf, nome, sobrenome, nascimento) values 
-('111.111.111-11', 'Jair', 'Silva', '1980-01-01'),
-('222.222.222-22', 'Jafoi', 'Solza', '1990-02-02'),
-('333.333.333-33', 'Javai', 'Santos', '1985-03-03'),
-('444.444.444-44', 'Jaera', 'Silveira', '1982-04-04'),
+insert into Clientes(cpf, nome, sobrenome, nascimento) 
+values 
+('111.111.111-11', 'Jair', 'Silva','1980-01-01'),
+('222.222.222-22', 'Jafoi', 'Solza','1990-02-02'),
+('333.333.333-33', 'Javai', 'Santos','1985-03-03'),
+('444.444.444-44', 'Jaera', 'Silveira','1982-04-04'),
 ('555.555.555-55', 'Jacinto', 'Pena', '2005-05-05');
 
 select * from Clientes;
--- 2DES/1-pwbe/aula1/server.js
+++ 2DES/1-pwbe/aula1/server.js
@@ -3,43 +3,60 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
-// Conexão
+// Conexão com SGBD MySQL
 const con = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    database: 'lojiinha'
+    database: 'lojinha'
 });
 
-// criar uma função no back usando este modelo
-const teste = (req, res) => {
+// Rota de teste
+const teste = (req, res) => {    // criar uma função no back usando este modelo
     //let nome = req.query.nome;
-    res.send("Back-end respondendo "); // retorna
+    res.send("Back-end respondendo "); // retorna : resultado
 } 
 
-// Crud - Read
-const read = (req, res) => {
-    con,query("SELECT * FROM Clientes",(err, res) => {
+// CRUD - create
+const create = (req, res) => {
+    let cpf = req.query.cpf;
+    let nome = req.query.nome;
+    let sobrenome = req.query.sobrenome;
+    let nascimento = req.query.nascimento;
+    let query = `INSERT INTO clientes(cpf, nome, sobrenome, nascimento) VALUE`;
+    query += `('${cpf}', '${nome}', '${sobrenome}', '${nascimento}');`;
+    con.query(query,(err, result) => {
         if(err)
-            req.json(err);
+            res.json(err);
         else
-            res,json(result);
+            res.json(result);
     });
 }
 
-   // res.jason('Oi!');
+// Crud - Read
+const read = (req, res) => {
+    con.query("SELECT * FROM Clientes",(err, res) => {
+        if(err)
+            res.json(err);
+        else
+            res.json(result);
+    });
+}
 
+// res.jason('Oi!');
 
 // Configurações de saída - Front-end
 const app = express();
 app.use(express.jason());
 app.use(cors());
+
+// Rotas de saída - Front-end
 app.get("/", teste);
 app.get("/clientes",read);
 
-
 // Teste e porta no console
 //app.get("/", teste);
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log("Back-end respondendo na porta 3000"); // mensagem
 });
 
