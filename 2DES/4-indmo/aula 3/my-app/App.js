0863f92cdb879784c49f37c358f65cfad5e454c7
Carla-coder <cmartezanato@yahoo.com.br>
2024-02-09 11:26:36 -0300

    Att

--- 2DES/4-indmo/aula 3/my-app/App.js
+++ 2DES/4-indmo/aula 3/my-app/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <p></p>
-      <Text>PRIMEIRA AULA DE EXPO</Text>
+      <Text style={styles.text}>Primeira Aula de EXPO</Text>
       <p></p>
     
     <Image 
@@ -13,6 +13,9 @@ export default function App() {
     source={{
       uri: 'https://img.pikbest.com/png-images/20190918/cartoon-snail-loading-loading-gif-animation_2734139.png!sw800'}} />
     <StatusBar style="auto" />
+
+    <Text style={styles.text}>Senai 2024</Text>
+
   </View>
   );
 }
@@ -20,15 +23,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  logo: {
-    width: 166,
-    height: 158,
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+    fontFamily: 'Cursive',
+    alignContent: 'center',
+    marginBottom: 10,
+    marginTop: 10,
   },
 
+  logo: {
+    width: 200,
+    height:200,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
 });
