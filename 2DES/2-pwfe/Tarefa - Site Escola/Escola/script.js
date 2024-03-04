937567fb3d8255507db5786acbe2e74c95cd0543
Carla-coder <cmartezanaro@yahoo.com.br>
2024-03-04 11:25:05 -0300

    Att

---
+++ 2DES/2-pwfe/Tarefa - Site Escola/Escola/script.js
@@ -0,0 +1,18 @@
+// script.js
+
+$(document).ready(function () {
+    // Adiciona rolagem suave para os links da barra de navegação
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
