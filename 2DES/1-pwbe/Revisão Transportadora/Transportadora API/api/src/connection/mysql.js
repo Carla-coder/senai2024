d6776a641604f9c638451554e15027a472f31e66
Carla-coder <cmartezanaro@yahoo.com.br>
2024-03-05 10:25:33 -0300

    Att

---
+++ 2DES/1-pwbe/Revisão Transportadora/Transportadora API/api/src/connection/mysql.js
@@ -0,0 +1,10 @@
+// Arquivo de conexão com Banco de Dados
+
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'transportadora'
+});
+
+module.exports = con;
