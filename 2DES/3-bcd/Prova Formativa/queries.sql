60a5e76dc68a18fce2d6dfaac0ceb6bb67849718
Carla-coder <cmartezanaro@yahoo.com.br>
2024-03-26 11:30:50 -0300

    Att

---
+++ 2DES/3-bcd/Prova Formativa/queries.sql
@@ -0,0 +1,35 @@
+1. Crie uma consulta que liste os funcionários em ordem alfabética de nome:
+
+SELECT * FROM Funcionario ORDER BY funcionario ASC;
+
+2. Crie uma consulta que liste todos os funcionários e seus telefones:
+
+SELECT f.matricula, f.funcionario, t.telefone
+FROM Funcionario f
+LEFT JOIN Telefone t ON f.matricula = t.matricula
+ORDER BY f.matricula, t.telefone;
+
+
+3. Crie uma consulta que mostre somente os veículos da marca 'Fiat' ordenados pelo ano decrescente:
+
+SELECT * FROM Veiculo
+WHERE marca = 'Fiat'
+ORDER BY ano DESC;
+
+4. Crie uma consulta que mostre todos os dados dos veículos e a quantidade de manutenções realizadas em cada um:
+
+SELECT v.placa, v.modelo, v.marca, v.ano, COUNT(m.id_manutencao) AS quantidade_manutencoes
+FROM Veiculo v
+LEFT JOIN Manutencao m ON v.placa = m.placa
+GROUP BY v.placa, v.modelo, v.marca, v.ano
+ORDER BY v.placa;
+
+5. Crie uma consulta que mostre todas as manutenções acrescida do nome do funcionário que a realizou e o modelo do veículo e salve como uma visão chamada vw_todas_as_manutencoes:
+
+CREATE VIEW vw_todas_as_manutencoes AS
+SELECT m.*, f.funcionario, v.modelo
+FROM Manutencao m
+LEFT JOIN Funcionario f ON f.matricula = f.matricula
+LEFT JOIN Veiculo v ON m.placa = v.placa;
+
+SELECT * FROM vw_todas_as_manutencoes;
---
+++ 2DES/3-bcd/Prova Formativa/script.sql
@@ -0,0 +1,42 @@
+drop database if exists loja;
+create database loja CHARSET=UTF8 COLLATE=utf8_unicode_ci;
+use loja;
+
+-- Criação da tabela Veículo
+CREATE TABLE Veiculo (
+    placa VARCHAR(20) PRIMARY KEY,
+    modelo VARCHAR(50),
+    marca VARCHAR(50),
+    ano INT
+);
+
+-- Criação da tabela Manutenção
+CREATE TABLE Manutencao (
+    id_manutencao INT PRIMARY KEY,
+    inicio_manutencao DATE,
+    fim_manutencao DATE,
+    descricao VARCHAR(100),
+    placa VARCHAR(20),
+    FOREIGN KEY (placa) REFERENCES Veiculo(placa)
+);
+
+-- Criação da tabela Funcionário
+CREATE TABLE Funcionario (
+    matricula INT PRIMARY KEY,
+    funcionario VARCHAR(100)
+    
+);
+
+-- Criação da tabela Telefone
+CREATE TABLE Telefone (
+    matricula INT,
+    telefone VARCHAR(20),
+    PRIMARY KEY (matricula, telefone),
+    FOREIGN KEY (matricula) REFERENCES Funcionario(matricula)
+);
+
+describe Veiculo;
+describe Manutencao;
+describe Funcionario;
+describe Telefone;
+show tables;
