f2f4ab3c9a5d7c15119db0d3e55f4ae830ed60e1
Carla-coder <cmartezanato@yahoo.com.br>
2024-04-29 18:41:12 -0300

    Att

--- 2DES/2-pwfe/aula 12/pedra_papel_tesoura/script.js
+++ 2DES/2-pwfe/aula 12/pedra_papel_tesoura/script.js
@@ -1,24 +1,24 @@
-var regras = document.getElementsByClassName("regras")[0];
+let regras = document.getElementsByClassName("regras")[0];
 function mostraregras() {
   regras.style.display = "flex";
 }
 function fecharegras() {
   regras.style.display = "none";
 }
-var jogo = document.getElementsByClassName("jogo")[0];
-var selecao = document.getElementsByClassName("gameplay")[0];
-var test = document.getElementsByClassName("test")[0];
-var opcao = document.getElementsByClassName("opcao")[0];
-var pedra = document.getElementsByClassName("pedra")[0];
-var tesoura = document.getElementsByClassName("tesoura")[0];
-var papel = document.getElementsByClassName("papel")[0];
-var test1 = document.getElementsByClassName("test1")[0];
-var resultado = document.getElementsByClassName("result")[0]
-var refs = [pedra, papel, tesoura];
-var pontuacao = document.getElementsByClassName('pontuacao')[0]
-var playAgain = document.getElementsByClassName('fim')[0]
-var score = 0
-var buttonRegras = document.getElementsByClassName('regras-button')[0]
+let jogo = document.getElementsByClassName("jogo")[0];
+let selecao = document.getElementsByClassName("gameplay")[0];
+let test = document.getElementsByClassName("test")[0];
+let opcao = document.getElementsByClassName("opcao")[0];
+let pedra = document.getElementsByClassName("pedra")[0];
+let tesoura = document.getElementsByClassName("tesoura")[0];
+let papel = document.getElementsByClassName("papel")[0];
+let test1 = document.getElementsByClassName("test1")[0];
+let resultado = document.getElementsByClassName("result")[0]
+let refs = [pedra, papel, tesoura];
+let pontuacao = document.getElementsByClassName('pontuacao')[0]
+let playAgain = document.getElementsByClassName('fim')[0]
+let score = 0
+let buttonRegras = document.getElementsByClassName('regras-button')[0]
 function jogar(num) {
   jogo.style.cssText = "animation: opacidade 0.5s linear;animation-direction: reverse;"
   
@@ -47,27 +47,24 @@ buttonRegras.style.display = "none"
       break;
   }
 
-  var random = Math.floor(Math.random() * 3);
-  var house = refs[random];
+  let random = Math.floor(Math.random() * 3);
+  let house = refs[random];
   test1.innerHTML = house.outerHTML;
   test1.value = house.value;
 
-
-
 // PARTIDA
-
 setTimeout(() => {
   Resolucao()
 }, 500);
 }
 function Resolucao(){
   if(test.value == "pedra" && test1.value == "tesoura" || test.value == "tesoura" && test1.value == "papel"|| test.value == "papel" && test1.value == "pedra"){
-    resultado.innerHTML = "YOU WIN";
+    resultado.innerHTML = "VOCÊ GANHOU";
     score += 1;
     pontuacao.innerHTML = score;
     test.style.cssText = "animation: luzDeFundo 1.5s ease infinite;";
    }else if(test.value == "pedra" && test1.value == "papel" || test.value == "tesoura" && test1.value == "pedra"|| test.value == "papel" && test1.value == "tesoura"){
-     resultado.innerHTML = "YOU LOSE";
+     resultado.innerHTML = "VOCÊ PERDEU";
      test1.style.cssText = "animation: luzDeFundo 1.5s ease infinite;";
    }else{
     resultado.innerHTML = "DRAW";
