e2dce0491cdb0ea61c00b118d76c992ec883a065
Carla-coder <cmartezanato@yahoo.com.br>
2024-05-05 17:41:10 -0300

    Att

--- 2DES/1-pwbe/Tarefa - DP Mediator/script.js
+++ 2DES/1-pwbe/Tarefa - DP Mediator/script.js
@@ -1,49 +1,51 @@
-document.getElementById('user-input').addEventListener('keypress', function(event) {
+document
+  .getElementById('user-input')
+  .addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
-        sendMessage();
+      sendMessage()
     }
-});
+  })
 
 // Mediator pattern implementation
-const ChatMediator = (function() {
-  let messages = [];
+const ChatMediator = (function () {
+  let messages = []
 
   return {
-      sendMessage: function(message) {
-          messages.push(message);
-          ChatUI.updateChat();
-      },
-      getMessages: function() {
-          return messages;
-      }
-  };
-})();
+    sendMessage: function (message) {
+      messages.push(message)
+      ChatUI.updateChat()
+    },
+    getMessages: function () {
+      return messages
+    }
+  }
+})()
 
 // UI controller
-const ChatUI = (function() {
-  const chatBox = document.getElementById('chat-box');
+const ChatUI = (function () {
+  const chatBox = document.getElementById('chat-box')
 
   return {
-      updateChat: function() {
-          chatBox.innerHTML = '';
-          ChatMediator.getMessages().forEach(message => {
-              const messageElement = document.createElement('div');
-              messageElement.textContent = message;
-              chatBox.appendChild(messageElement);
-          });
-      }
-  };
-})();
+    updateChat: function () {
+      chatBox.innerHTML = ''
+      ChatMediator.getMessages().forEach(message => {
+        const messageElement = document.createElement('div')
+        messageElement.textContent = message
+        chatBox.appendChild(messageElement)
+      })
+    }
+  }
+})()
 
 // Controller
-function sendMessage() {
-  const userInput = document.getElementById('user-input');
-  const message = userInput.value;
+function sendMessage () {
+  const userInput = document.getElementById('user-input')
+  const message = userInput.value
   if (message.trim() !== '') {
-      ChatMediator.sendMessage(message);
-      userInput.value = '';
+    ChatMediator.sendMessage(message)
+    userInput.value = ''
   }
 }
 
 // Initial chat display
-ChatUI.updateChat();
+ChatUI.updateChat()
