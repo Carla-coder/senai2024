32a3f07da9f7d491e16bb32171650ee5de69172d
Carla-coder <cmartezanaro@yahoo.com.br>
2024-06-03 11:24:34 -0300

    Att

--- 2DES/2-pwfe/aula 14/Genius/script.js
+++ 2DES/2-pwfe/aula 14/Genius/script.js
@@ -1,69 +1,107 @@
-const { ToggleButton } = require("react-bootstrap");
-
-const buttonElements = document.querySelectorAll('.clickable');
-const controlElement = document.querySelector('.control .background');
-const controlStatusElement = document.querySelector('control p');
-const scoreElement = document.querySelector('.score');
-const highScoreElenent = document.querySelector('.high-score');
-const container = document.querySelector('.container');
-const nightModeButton = document.querySelector('.night-mode-button');
-
-let roundAnswers = [];
-let playerAnswers = [];
-let difficulty = 4;
-let intervalDecrease = 0;
-let score = 0;
-let highScore = 0;
-
-let waitingPlayerAnswer = false;
-let canStartRound = true;
-
-const getRandomValueArray = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
+const revampDifficulty = (toIncrease) => {
+    if (toIncrease) {
+        difficulty++;
+        intervalDecrease = (intervalDecrease < 800) ? intervalDecrease + 10 : intervalDecrease;
+    } else {
+        difficulty = 4;
+        intervalDecrease = 0;
+    }
 };
 
-const displaySequence = (index) => {
-    const element = roundAnswers[index];
+const updateScore = () => {
+    scoreElement.innerHTML = score;
+    highScoreElement.innerHTML = (highScore > 0) ? highScore : '-';
+};
 
-    setTimeout(() => {
-        element.classList.add('active');
+const processAnswers = () => {
+    waitingPlayerAnswer = false;
+
+    toggleButtonsCursorStyle();
+
+    let allCorrect = true;
+
+    for (let i in roundAnswers) {
+        const properAnswer = roundAnswers[i];
+        const playerAnswer = playerAnswers[i];
+
+        if (properAnswer !== playerAnswer) {
+            allCorrect = false;
+        }
+    }
+
+    if (allCorrect) {
+        controlElement.style.cursor = 'pointer';
+        controlElement.style.backgroundColor = 'green';
+
+        controlStatusElement.innerHTML = 'ACERTOU';
 
         setTimeout(() => {
-            element.classList.remove('active');
-            index++;
+            callRound()
+        }, 1500);
+    } else {
+        controlElement.style.cursor = 'pointer';
+        controlElement.style.backgroundColor = 'red';
 
-            if (index < roundAnswers.lenght){
-                displaySequence(index);
-            }else {
-                waitingPlayerAnswer = true;
-                
-                controlElement.style.backgroundColor = 'lightblue';
-                controlStatusElement.innerHtml = 'Reproduzir';
+        controlStatusElement.innerHTML = 'RECOMEÇAR';
 
-                toggleButtonsCursorStyle();
-            }
-        }, 1000 - intervalDecrease)
-    }, 1000 - intervalDecrease)
-};
+        highScore = (score > highScore) ? score : highScore;
 
-const callRound = () => {
-    playerAnswers = [];
-
-    controlElement.style.cursor = 'auto';
-    controlElement.style.backgroundColor = 'yellow';
-    controlStatusElement.innerHTML = 'Observe';
-
-    const loopLimit = difficulty - roundAnswers.length;
-
-    for(let i = 0; i < loopLimit; i++) {
-        const randomValue = getRandomValueArray(buttonElements);
-
-        roundAnswers.push(randomValue);
+        canStartRound = true;
     }
-    displaySequence(0);
+
+    score = (allCorrect) ? score + 1 : score;
+    score = (allCorrect) ? score : 0;
+
+    updateScore();
+    revampDifficulty(allCorrect);
 };
-const toggleButtonsCursorStyle = () => {
-    for (let element of buttonElements) {
-        element.style.cursor = element.style.cursor === 'pointer' ? '': 'pointer';
+
+const processClick = (element) => {
+    if (!waitingPlayerAnswer) {
+        return;
+    }
+
+    playerAnswers.push(element);
+    element.classList.add('active');
+
+    setTimeout(() => {
+        element.classList.remove('active');
+    }, 750);
+
+    const i = playerAnswers.length - 1;
+
+    if (playerAnswers[i] !== roundAnswers[i] || playerAnswers.length === roundAnswers.length) {
+        processAnswers();
     }
 };
+
+controlElement.onclick = () => {
+    if (canStartRound) {
+        callRound();
+
+        canStartRound = false;
+    }
+};
+
+nightModeButton.onclick = () => {
+    container.classList.toggle('night-mode');
+    container.classList.toggle('day-mode');
+};
+
+for (let element of buttonElements) {
+    element.onclick = () => {
+        processClick(element);
+    };
+
+    element.onmouseenter = () => {
+        if (waitingPlayerAnswer && !element.classList.contains('active')) {
+            element.classList.add('hover');
+        }
+    };
+
+    element.onmouseleave = () => {
+        if (waitingPlayerAnswer && !element.classList.contains('active')) {
+            element.classList.remove('hover');
+        }
+    }
+}
