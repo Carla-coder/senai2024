fb4987712cc86846d61f57be58b73d665906eb2e
Carla-coder <you@example.comcmartezanato@yahoo.com.br>
2024-08-22 11:19:00 -0300

    Att

--- 3DES/2- PPDMO/aula03/Pets/pets/firebaseconfig.js
+++ 3DES/2- PPDMO/aula03/Pets/pets/firebaseconfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyCNWaQx8BWD3n20PEPDRUq5y0_WviyGPcQ",
     authDomain: "pets-812a6.firebaseapp.com",
@@ -15,7 +16,6 @@ const firebaseConfig = {
    const app = initializeApp(firebaseConfig);
 
    const db = getFirestore(app);
- 
    const auth = getAuth(app);
- 
+  
      export {db, auth};
